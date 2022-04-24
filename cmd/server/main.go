93dd88b9d6c74bfd7c1e90919bc9443f51d1cd86
OlesyaBelochka <OlesyaBelle@gmail.com>
2022-04-24 21:54:20 +0300

    Increment 3 (change server main.go = part3)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	mux := chi.NewRouter()
+	//mux := chi.NewMux()
+	//
+	//// зададим встроенные middleware, чтобы улучшить стабильность приложения
+	//mux.Use(middleware.RequestID)
+	//mux.Use(middleware.RealIP)
+	//mux.Use(middleware.Logger)
+	//mux.Use(middleware.Recoverer)
+	//
+	//mux.Get("/", HandleGetAllMetrics)
+	//mux.Get("/value/{mType}/{mName}", HandleGetMetric)
+	//mux.Get("/value/{mType}/{mName}/{mValue}", func(w http.ResponseWriter, r *http.Request) {
+	//
+	//	sendStatus(w, 700)
+	//
+	//})
+	//
+	////mux.Get("/{anystring}", func(w http.ResponseWriter, r *http.Request) {
+	////	sendStatus(w, 600)
+	////	fmt.Println("anystring")
+	////})
+	//
+	//mux.Post("/update/{mType}/{mName}/{mValue}", HandleUpdateMetrics)
+	////localhost:8080
+	//log.Fatal(http.ListenAndServe("127.0.0.1:8080", mux))
+	////log.Fatal(http.ListenAndServe(":8080", mux))
+	//
+
+	mux := chi.NewMux()
 
 	// зададим встроенные middleware, чтобы улучшить стабильность приложения
-	mux.Use(middleware.RequestID)
-	mux.Use(middleware.RealIP)
-	mux.Use(middleware.Logger)
-	mux.Use(middleware.Recoverer)
+	//mux.Use(middleware.RequestID)
+	//mux.Use(middleware.RealIP)
+	//mux.Use(middleware.Logger)
+	//mux.Use(middleware.Recoverer)
 
 	mux.Get("/", HandleGetAllMetrics)
 	mux.Get("/value/{mType}/{mName}", HandleGetMetric)
@@ -24,11 +50,6 @@ func main() {
 		sendStatus(w, 700)
 
 	})
-
-	//mux.Get("/{anystring}", func(w http.ResponseWriter, r *http.Request) {
-	//	sendStatus(w, 600)
-	//	fmt.Println("anystring")
-	//})
 
 	mux.Post("/update/{mType}/{mName}/{mValue}", HandleUpdateMetrics)
 
