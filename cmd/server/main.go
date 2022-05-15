15d92bb62000a24a2107968ba9a2008655dd0024
OlesyaBelochka <OlesyaBelle@gmail.com>
2022-05-15 21:29:14 +0300

    check flags #5

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -49,9 +49,9 @@ func setFlags() {
 	//flag.Parse()
 	if !fRstor {
 		fmt.Println("Server set flag Restore", fRstor)
-		//if !variables.Conf.Restore {
-		variables.Conf.Restore = fRstor == variables.Conf.Restore
-		//}
+		if !variables.Conf.Restore {
+			variables.Conf.Restore = fRstor
+		}
 
 	}
 
@@ -79,7 +79,7 @@ func setFlags() {
 
 func main() {
 
-	setFlags()
+	//setFlags()
 
 	if variables.Conf.Restore {
 		fmt.Println("start RestoreMetricsFromFile")
